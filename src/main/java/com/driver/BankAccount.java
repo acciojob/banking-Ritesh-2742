e8a4e83bc223c2a284015ee3b3487544f80ad541
ritesh <rpoudiyal@gmail.com>
2022-12-31 13:12:57 +0530

    final

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -1,7 +1,5 @@
 package com.driver;
 
-import java.util.Random;
-
 public class BankAccount {
 
     private String name;
@@ -14,78 +12,55 @@ public class BankAccount {
         this.minBalance = minBalance;
     }
 
-    public String generateAccountNumber(int digits, int sum) throws Exception{
-        //Each digit of an account number can lie between 0 and 9 (both inclusive)
-        //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
-        //If it is not possible, throw "Account Number can not be generated" exception
-        String accountNumber = generateAc(digits,sum);
-
-        if(accountNumber.length()==0){
-            throw new Exception("Account Number can not be generated");
-        }
-        return accountNumber;
-    }
-
-    public void deposit(double amount) {
-        //add amount to balance
-        this.balance = balance+amount;
-    }
-
-    public void withdraw(double amount) throws Exception {
-        // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
-        if(this.balance < amount){
-            throw new Exception("Insufficient Balance");
-        }
-        this.balance -= amount;
+    public String getName() {
+        return name;
     }
 
     public double getBalance() {
         return balance;
-    }
-    private String generateAc(  int digit, int sum){
-        if(digit>sum){
-            return "";
-        }
-        int rem = sum % digit;
-        if(digit< sum/9){
-            return "";
-        }
-        if(digit == sum/9 && rem>0){
-            return "";
-        }
-        String ACNo = "";
-        while(sum > 0){
-            if(sum >= 9){
-                ACNo += "9";
-                sum -= 9;
-            }else{
-                ACNo+= sum;
-
-            }
-        }
-
-
-        return ACNo;
-    }
-
-    public String getName() {
-
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setBalance(double balance) {
-        this.balance = balance;
     }
 
     public double getMinBalance() {
         return minBalance;
     }
 
-    public void setMinBalance(double minBalance) {
-        this.minBalance = minBalance;
+    public String generateAccountNumber(int digits, int sum) throws Exception {
+        //Each digit of an account number can lie between 0 and 9 (both inclusive)
+        //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
+        //If it is not possible, throw "Account Number can not be generated" exception
+        if (sum > 9 * digits) {
+            throw new Exception("Account Number can not be generated");
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < digits; i++) {
+            if (sum >= 9) {
+                sb.append('9');
+                sum -= 9;
+            } else {
+                sb.append((char) ('0' + sum));
+                sum = 0;
+            }
+        }
+        return sb.toString();
     }
+
+    public void deposit(double amount) {
+        //add amount to balance
+        balance=balance +amount;
+
+    }
+
+    public void withdraw(double amount) throws Exception {
+        // Remember to throw "" exception, if the remaining amount would be less than minimum balance
+        if(this.balance-amount>=getMinBalance())
+        {
+            balance=balance-amount;
+        }
+        else {
+            throw new Exception("Insufficient Balance");
+        }
+
+    }
+
 }
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,11 +1,7 @@
 package com.driver;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
-        BankAccount bankAccount = new CurrentAccount("Sweata",6000,"AABC");
-        System.out.println(bankAccount.generateAccountNumber(3,30));
-        bankAccount.deposit(300);
-        bankAccount.withdraw(100);
     }
 }
